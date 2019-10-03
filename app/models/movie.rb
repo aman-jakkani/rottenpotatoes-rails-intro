1662d727c8d3d7039721413ae8ae0fb55934890f
EC2 Default User <ec2-user@ip-172-31-16-49.us-east-2.compute.internal>
2019-10-03 13:48:07 +0000

    trying part 2

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,8 @@
 class Movie < ActiveRecord::Base
-    scope :all_ratings, where(all_ratings: ['G','PG','PG-13','R','NC-17'])
+    attr_accessible :title, :rating, :description, :release_date
+
+    def self.all_ratings
+        Movie.select(:rating).uniq.map { |movie| movie.rating }.sort
+    end
     
 end
