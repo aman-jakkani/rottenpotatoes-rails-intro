ac0e26a12c71cbeb3fb7c8f03d3a0afdea7cbae8
EC2 Default User <ec2-user@ip-172-31-16-49.us-east-2.compute.internal>
2019-10-03 14:50:12 +0000

    slow changes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -21,12 +21,11 @@ class MoviesController < ApplicationController
     end
     
     @movies = Movie.with_ratings(ratingsfilters).order(session[:order])
-    #@movies = Movie.order(session[:order])
     @all_ratings = Movie.all_ratings
     @checked_filters = ratingsfilters
     @sortby = session[:order]
-    #redirect_to movies_path order: session[:order], ratings: session[:ratings] if (params[:order].nil? && session[:order]) || (params[:ratings].nil? && session[:ratings])
-    redirect_to movies_path order: session[:order] if (params[:order].nil? && session[:order])
+    redirect_to movies_path order: session[:order], ratings: session[:ratings] if (params[:order].nil? && session[:order]) || (params[:ratings].nil? && session[:ratings])
+    #redirect_to movies_path order: session[:order] if (params[:order].nil? && session[:order])
   end
   
   def new
