f25d05db907453b90a64101a2234557a21633dc6
EC2 Default User <ec2-user@ip-172-31-16-49.us-east-2.compute.internal>
2019-10-03 03:55:27 +0000

    part 2 test

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,7 @@
 class Movie < ActiveRecord::Base
-    attr_accessor :all_ratings
     
     def all_ratings
-        @all_ratings = ['G','PG','PG-13','R','NC-17']
+        self[:all_ratings] = ['G','PG','PG-13','R','NC-17']
     end
     
 end
